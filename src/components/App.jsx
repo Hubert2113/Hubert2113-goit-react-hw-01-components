b677b50d880d3d00ef7ede415221b029271dd497
Hubert2113 <htumialiss@gmail.com>
2022-05-25 04:33:09 +0200

    sdrfgh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { PageContainer } from "./App.styles";
+
+export const App = ({children}) => {
+  return <PageContainer>{children}</PageContainer>;
 };
